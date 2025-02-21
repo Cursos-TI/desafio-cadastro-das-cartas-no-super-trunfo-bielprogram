40b6a113de42ac8bc4ecd1139974d2f9d497ac1c
bielprogram <gabrielap1910@gmail.com>
2025-02-21 21:21:33 +0000

    eu alterei o printf

--- CartasSuperTrunfo.1.c
+++ CartasSuperTrunfo.1.c
@@ -1,15 +1,50 @@
 #include <stdio.h>
 
 int main(){
-    printf("desafio cartas super trunfo!/n");
+    printf("desafio cartas super trunfo!: ");
+
+    
+    float populacao;
+    int turismo;
+    float area;
+    float PIB;
+    char estado[50];
+    char codigo[50];
+    char cidade[50];
+
+    
+    
+    printf("Digite o estado (sigla, ex: SP): ");
+    scanf("%2s",estado);
+    
 
 
+    printf("digite o codigo:\n ");
+    scanf("%s",codigo);
+    
+
+    printf("digite a cidade:\n ");
+    scanf("%s",cidade);
+    
+
+    printf("digite a populacao:\n ");
+    scanf("%f",&populacao);
+    
+
+    printf("digite a area:\n ");
+    scanf("%f",&area);
 
 
+    printf("digite o pib:\n ");
+    scanf("%f",&PIB);
+    
+    printf("digite o turismo:\n ");
+    scanf("%i",&turismo);
+    
+    return 0;
 
 
-
-
+ 
 
 
 }
